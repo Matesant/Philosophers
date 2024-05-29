500022fd09ea94ed1362161652b86b20671c16ff
mateus <mateussantos1528@hotmail.com>
2024-05-29 19:44:54 -0300

    WIP: Segfault solved, deadlock on the way

--- philo_bonus/src_bonus/forks_creation/dinning_bonus.c
+++ philo_bonus/src_bonus/forks_creation/dinning_bonus.c
@@ -6,7 +6,7 @@
 /*   By: matesant <matesant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 17:03:23 by matesant          #+#    #+#             */
-/*   Updated: 2024/05/29 19:15:05 by matesant         ###   ########.fr       */
+/*   Updated: 2024/05/29 19:41:00 by matesant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ void	ft_dinning_hall(t_philo *philo)
 void	ft_philo_sit_down(t_philo *philo)
 {
 	philo->pid = fork();
+	philo->last_meal = ft_get_ms();
 	if (philo->pid < 0)
 	{
 		perror("fork failed");
