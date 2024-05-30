42e0db084e789b32b69397739b397cfaf2275815
mateus <mateussantos1528@hotmail.com>
2024-05-30 15:05:24 -0300

    Refactoring

--- philo_bonus/src_bonus/forks_creation/dinning_bonus.c
+++ philo_bonus/src_bonus/forks_creation/dinning_bonus.c
@@ -6,13 +6,14 @@
 /*   By: matesant <matesant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 17:03:23 by matesant          #+#    #+#             */
-/*   Updated: 2024/05/30 01:02:25 by matesant         ###   ########.fr       */
+/*   Updated: 2024/05/30 15:02:27 by matesant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers_bonus.h"
 
 void	ft_philo_sit_down(t_philo *philo);
+void	ft_check_failed_fork(t_philo *philo);
 
 void	ft_dinning_hall(t_philo *philo)
 {
@@ -33,11 +34,16 @@ void	ft_philo_sit_down(t_philo *philo)
 {
 	philo->pid = fork();
 	philo->last_meal = ft_get_ms();
+	ft_check_failed_fork(philo);
+	if (philo->pid == 0)
+		ft_im_hungry(philo);
+}
+
+void	ft_check_failed_fork(t_philo *philo)
+{
 	if (philo->pid < 0)
 	{
 		perror("fork failed");
 		exit(1);
 	}
-	else if (philo->pid == 0)
-		ft_im_hungry(philo);
 }
